b8706236c6f5325bf8196f711a27e183fecf7006
daygon2007 <daygon07@gmail.com>
2016-03-18 15:46:32 -0500

    Fixed issue with skrollr not allowing to scroll on mobile

--- _js/skrollr-init.js
+++ _js/skrollr-init.js
@@ -1,2 +1,6 @@
 skrollr.init({
+    mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
 });
--- theme/header.php
+++ theme/header.php
@@ -21,30 +21,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'jonathon-harrelson-v3' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jonathon-harrelson-v3' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+    <?php include('navbar.php'); ?>
+<div id="page skrollr-body" class="site">
+    <header>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
