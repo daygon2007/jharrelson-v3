63f264679f05e49ec1739fe5a424a734300724e9
daygon2007 <daygon07@gmail.com>
2016-03-21 12:55:29 -0500

    update to intro page

--- theme/page-intro.php
+++ theme/page-intro.php
@@ -7,14 +7,11 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
-    <script>
-        if (screen.width < 600 || screen.height < 200) {
-            window.location = '<?php bloginfo('url'); ?>/home'
-        }
-    </script>
+    
 </head>
 <body>
-    <div id="start" class="hidden-xs hidden-sm">
+    <div id="skrollr-body"></div>
+    <div id="start">
 	<div class="intro-computer" data-0="transform:scale(1.75,1.75);" data-300="transform:scale(.5,.5); top:0%; opacity:1; z-index:9999;" data-600="top:-100%; opacity:0; z-index:1;">
 		<img src="<?php bloginfo(template_directory); ?>/images/intro.png" alt="Jonathon Harrelson desktop background image">
 		<div class="logo">
@@ -29,7 +26,7 @@
 </div>
 <!-- End Start Screen -->
 
-<div id="portfolio-section" data-300="top:100%; opacity:0;" data-600="top:0%; opacity:1;" data-7900="opacity:1; top:0%;" data-8200="top:-100%; opacity:0;" class="hidden-xs hidden-sm">
+<div id="portfolio-section" data-300="top:100%; opacity:0;" data-600="top:0%; opacity:1;" data-7900="opacity:1; top:0%;" data-8200="top:-100%; opacity:0;">
 	<div class="content-area">
 		<p data-600="opacity:0;" data-900="opacity:1;" data-1200="opacity:1;" data-1500="opacity:0;">I Create</p>
 		<p data-1500="opacity:0;" data-1800="opacity:1;" data-2100="opacity:1;" data-2400="opacity:0;">A Journey</p>
@@ -46,12 +43,13 @@
     <h2 class="caps" data-8300="opacity:0;" data-8900="opacity:1;" data-9600="opacity:1;" data-9900="opacity:0;">Web Design and Development is my Passion</h2>
     <h3 data-8600="opacity:0;" data-8900="opacity:1;" data-9600="opacity:1;" data-9900="opacity:0;">I Love Turning this...</h3>
     <h3 data-10500="opacity:0;" data-10800="opacity:1;" data-11100="opacity:1;" data-11400="opacity:0;">Into this...</h3>
-    <img src="http://placehold.it/1920x1080?text=Awesome+Site+Image" data-11400="opacity:0;" data-11700="opacity:1;" data-12700="opacity:1;" data-13000="opacity:0;">
+    <img src="http://placehold.it/1920x1080?text=Awesome+Site+Image+For+Desktop" data-11400="opacity:0;" data-11700="opacity:1;" data-12700="opacity:1;" data-13000="opacity:0;" class="hidden-sm hidden-xs">
+    <img src="http://placehold.it/375x667?text=Awesome+Site+Image+For+Mobile" data-11400="opacity:0;" data-11700="opacity:1;" data-12700="opacity:1;" data-13000="opacity:0;" class="visible-sm visible-xs">
     <h3 data-13000="opacity:0;" data-13300="opacity:1;">Using these programming languages and platforms:</h3>
     
 </div>
 
-<div id="contact-section" class="hidden-xs hidden-sm">
+<div id="contact-section">
 	
 </div>
 <!-- End Contact Section -->
